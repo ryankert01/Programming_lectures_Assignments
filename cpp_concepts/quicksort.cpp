83d376e8643f5e786a3d3c6dae5563db8c48708b
ryankert01 <ryan980053@gmail.com>
2022-03-22 11:25:31 +0800

    quicksort' '

--- cpp_concepts/quicksort.cpp
+++ cpp_concepts/quicksort.cpp
@@ -3,6 +3,11 @@
 using size_type = unsigned int;
 
 template< class T >
+bool sortRule(T& a, T& b){
+    return a < b;
+}
+
+template< class T >
 void printArray(T arr[], size_type size){
     for(size_type i = 0; i < size; i++)
         std::cout << arr[i] << " ";
@@ -21,7 +26,7 @@ size_type partition(T arr[], size_type low, size_type high){
     T pivot = arr[high];
     size_type i = low - 1;
     for(size_type j = low; j <= high - 1; j++){
-        if(arr[j] < pivot){
+        if(sortRule(arr[j], pivot)){
             i++;
             swap(&arr[i], &arr[j]);
         }
@@ -39,6 +44,8 @@ void quickSort(T arr[], size_type low, size_type high){
     }
 
 }
+
+
 
 int main(){
     int arr[] = {9,1,4,2,6,7,8,3,4};
